f4693575e813afb740cf51b97fec23914db20f4a
Denisa <Denisa@DESKTOP-21R9RUQ>
2022-11-08 21:52:52 +0200

    Lab5

--- Data/Todea_Denisa_Lab2Context.cs
+++ Data/Todea_Denisa_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Todea_Denisa_Lab2.Data
         public DbSet<Todea_Denisa_Lab2.Models.Author> Authors { get; set; }
 
         public DbSet<Todea_Denisa_Lab2.Models.Category> Category { get; set; }
+
+        public DbSet<Todea_Denisa_Lab2.Models.Member> Member { get; set; }
+
+        public DbSet<Todea_Denisa_Lab2.Models.Borrowing> Borrowing { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Todea_Denisa_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
+using Todea_Denisa_Lab2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Todea_Denisa_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Todea_Denisa_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Todea_Denisa_Lab2Context' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Todea_Denisa_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Todea_Denisa_Lab2Context' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
+
 
 var app = builder.Build();
 
@@ -22,6 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
