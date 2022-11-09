fcd9d04052fba14f0c296e19daf608d798381934
Denisa <Denisa@DESKTOP-21R9RUQ>
2022-11-09 23:59:48 +0200

    New Lab4

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Todea_Denisa_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
