1b494449bc112d5aa70f9cfd1a8849947858ded1
Denisa <Denisa@DESKTOP-21R9RUQ>
2022-11-15 16:32:39 +0200

    temalab5

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ using Todea_Denisa_Lab2.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+});
 builder.Services.AddDbContext<Todea_Denisa_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Todea_Denisa_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Todea_Denisa_Lab2Context' not found.")));
 
@@ -16,6 +21,7 @@ builder.Services.AddDbContext<LibraryIdentityContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Todea_Denisa_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Todea_Denisa_Lab2Context' not found.")));
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
  .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 
